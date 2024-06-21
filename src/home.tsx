03dfa4b0a71ef61a792e2ad63bbd890b506b0d1d
duduborges <duduborges333969@gmail.com>
2024-06-21 12:43:22 -0300

    atualizado

--- src/home.tsx
+++ src/home.tsx
@@ -11,13 +11,15 @@ import logo from "../Drive_sign_logo_semfundo.png"
 import { useEffect, useState } from "react";
 import sobre from "../sobrenos.jpeg"
 import objetivo from "../objetivo.jpeg"
-import a from "../img/kit-c-multimidia-servico.png"
-import b from "../img/kit-c-multimidia.png"
-import c from "../img/kit-sem-nada.png"
-import d from "../img/kit-sem-nada-c-servico.png"
+import a from "../img/kit1.png"
+import b from "../img/kit2.png"
+// import c from "../img/kit-sem-nada.png"
+// import d from "../img/kit-sem-nada-c-servico.png"
 import planoBarato from "../img/plano.png"
 import planoCaro from "../img/planoPremium.png"
 import { FaInstagram, FaYoutube } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { BsWhatsapp } from "react-icons/bs";
 
 
 
@@ -74,20 +76,26 @@ export default function Home() {
                     </Navbar.Link>
                     <Navbar.Link className={`text-[16px] ${activeLink === 'plans' ? 'text-secondaryColor' : 'text-gray-500'}`} name="plans"
                         onClick={handleClick} href="#plans">Nossos planos</Navbar.Link>
-                    <Navbar.Link className={`text-[16px] ${activeLink === 'products' ? 'text-secondaryColor' : 'text-gray-500'}`} name="products"
+                    <Navbar.Link className={`text-[16px] ${activeLink === 'product' ? 'text-secondaryColor' : 'text-gray-500'}`} name="product"
                         onClick={handleClick} href="#product">Nossos produtos</Navbar.Link>
-                    <Navbar.Link className={`text-[16px] ${activeLink === 'contact' ? 'text-secondaryColor' : 'text-gray-500'}`} name="contact"
-                        onClick={handleClick} href="#contact">Contate-nos</Navbar.Link>
+                    <Navbar.Link className={`text-[16px] ${activeLink === 'contato' ? 'text-secondaryColor' : 'text-gray-500'}`} name="contato"
+                        onClick={handleClick} href="#contato">Contate-nos</Navbar.Link>
                 </Navbar.Collapse>
             </Navbar>
         </header >
         <main className="w-full z-1">
 
             <section id="about">
-                <div className="w-9/12 m-auto flex justify-between">
+                <div className="w-9/12 m-auto flex justify-evenly">
                     <div className="w-8/12">
                         <h1 className="text-center text-3xl font-semibold font-mono ">Quem somos?</h1>
-                        <p className="text-xl font-serifl">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati quaerat velit facere odit, deserunt maxime nostrum laborum, ut illo blanditiis explicabo eligendi unde rerum aliquid labore dolore. Minus, temporibus reprehenderit. consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, nisi sunt labore aliquam perferendis ullam atque nulla maxime illo praesentium molestias inventore quia amet ipsam nobis quos repudiandae perspiciatis deserunt? adipisicing elit. Aliquam vel, est magnam aliquid quibusdam eligendi. Dolorem maiores voluptatum illum enim! Repudiandae voluptate quisquam veniam voluptas dicta iusto dolore vero quo!</p>
+                        <p className="text-xl text-justify font-serifl tracking-tighter w-3/4 m-auto">
+                            Nós somos uma equipe apaixonada por tornar o trânsito mais acessível para todos.
+                            Nosso projeto visa melhorar a segurança e a inclusão de pessoas com deficiência auditiva.
+                            Desenvolvemos um aplicativo inovador que se integra com sensores e dispositivos Arduino para
+                            ajudar os usuários a identificar a origem dos sons no trânsito. Acreditamos que a tecnologia pode ser
+                            uma aliada poderosa na busca por um mundo mais inclusivo e seguro para todos.
+                        </p>
                     </div>
                     <div>
                         <img src={sobre} className="bg-white w-[25em] rounded-md h-[25rem]" alt="Foto da equipe" />
@@ -97,31 +105,37 @@ export default function Home() {
 
             <section >
 
-                <div className="w-9/12 m-auto my-11 flex justify-between">
+                <div className="w-9/12 m-auto my-11 flex justify-evenly">
                     <div>
                         <img src={objetivo} className="w-[25em] h-[25rem] rounded-md bg-white" alt="Foto da equipe" />
                     </div>
                     <div className="w-8/12">
                         <h1 className="text-center text-3xl font-semibold font-mono ">Nosso objetivo</h1>
-                        <p className="text-xl font-serifl">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati quaerat velit facere odit, deserunt maxime nostrum laborum, ut illo blanditiis explicabo eligendi unde rerum aliquid labore dolore. Minus, temporibus reprehenderit. consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, nisi sunt labore aliquam perferendis ullam atque nulla maxime illo praesentium molestias inventore quia amet ipsam nobis quos repudiandae perspiciatis deserunt? adipisicing elit. Aliquam vel, est magnam aliquid quibusdam eligendi. Dolorem maiores voluptatum illum enim! Repudiandae voluptate quisquam veniam voluptas dicta iusto dolore vero quo!</p>
+                        <p className="text-xl font-serifl text-justify tracking-tighter w-3/4 m-auto">
+                            Estamos comprometidos em aprimorar constantemente nosso aplicativo e expandir sua funcionalidade. Além de identificar a origem
+                            dos sons no trânsito, estamos explorando maneiras de fornecer informações em tempo real sobre semáforos, obstáculos e condições
+                            de tráfego. Queremos que nosso aplicativo seja uma ferramenta completa para a comunidade, capacitando todos a se movimentarem
+                            com segurança e confiança. Se você compartilha de nossa paixão pela acessibilidade e segurança no trânsito, junte-se a nós!
+                            Estamos sempre abertos a novas ideias, colaborações e parcerias. Entre em contato conosco e faça parte dessa missão.
+                        </p>
                     </div>
 
                 </div>
             </section>
 
-            <section id="plans" className="pt-20 pb-10">
+            <section id="plans" className="pt-10 pb-10">
                 <div className="text-center text-5xl lexend-tera py-10">
                     <h2>Nossos planos</h2>
                 </div>
                 <div className="flex justify-evenly w-10/12 m-auto">
                     <div className="flex flex-col cursor-pointer w-1/3 m-auto justify-center align-middle items-center">
-                        <div className="w-96 py-5 minhaimg"><img className="shadoww" src={planoBarato} alt="" /></div>
+                        <div className="w-96 py-5 minhaimg"><img className="shadoww h-1/5" src={planoBarato} alt="" /></div>
                         <div className="text-center lexend-tera w-full "><p className="text-2xl">Plano BASIC</p><p className="text-xl text-green-300">R$ 39,90/Mes</p>
                             <p className="text-sm text-left mt-2">Benefícios: <p>Detectar sensores, ligações diretamente do aplicativo, gravações do trajeto</p> </p>
                         </div>
                     </div>
                     <div className="flex flex-col cursor-pointer w-1/3 m-auto justify-center align-middle items-center">
-                        <div className="w-96 py-7 minhaimg"><img className="shadoww" src={planoCaro} alt="" /></div>
+                        <div className="w-96 py-7 minhaimg"><img className="shadoww h-1/5" src={planoCaro} alt="" /></div>
                         <div className="  lexend-tera w-full text-center "><p className="text-2xl">Plano PREMIUM</p><p className="text-xl text-green-300">R$ 59,90/Mes</p>
                             <p className="text-sm text-left mt-2">Beneficios: <p>Detectar sensores, ligações diretamente do aplicativo, gravações do trajeto, manual em libras, guia de manutenção</p></p>
                         </div>
@@ -130,33 +144,33 @@ export default function Home() {
             </section>
 
             <section id="product">
-                <Carousel className="pb-32 w-10/12 m-auto">
+                <Carousel className="pb-32 pt-10  w-10/12 m-auto">
                     <div className="text-center text-5xl lexend-tera py-10">
                         <h2>Nossos produtos</h2>
                     </div>
                     <CarouselContent>
                         <CarouselItem className="basis-1/3">
                             <div className="flex flex-col cursor-pointer justify-center align-middle items-center">
-                                <div className="w-96 py-7 minhaimg"><img className="shadoww" src={c} alt="" /></div>
-                                <div className="text-center lexend-tera w-max"><p className="text-2xl">Kit Arduino + Sensor sônico</p><p className="text-xl text-green-300">R$ 600,00</p></div>
+                                <div className="w-96 py-7 minhaimg"><img className="shadoww" src={a} alt="" /></div>
+                                <div className="text-center lexend-tera w-max"><p className="text-2xl">Kit de Sensores</p><p className="text-xl text-green-300">R$ 600,00</p></div>
                             </div>
                         </CarouselItem>
                         <CarouselItem className="basis-1/3">
                             <div className="flex flex-col cursor-pointer justify-center align-middle items-center">
                                 <div className="w-96 py-7 minhaimg"><img className="shadoww" src={b} alt="" /></div>
-                                <div className="text-center lexend-tera w-full"><p className="text-2xl">Kit Arduino + Sensor sônico + Painel Multimídia</p><p className="text-xl text-green-300">R$ 1200,00</p></div>
+                                <div className="text-center lexend-tera w-full"><p className="text-2xl">Kit de Sensores + Painel Multimídia</p><p className="text-xl text-green-300">R$ 1200,00</p></div>
                             </div>
                         </CarouselItem>
                         <CarouselItem className="basis-1/3">
                             <div className="flex flex-col cursor-pointer justify-center align-middle items-center">
-                                <div className="w-96 py-7 minhaimg"><img className="shadoww" src={d} alt="" /></div>
-                                <div className="text-center lexend-tera w-full"><p className="text-2xl">Kit Arduino + Sensor sônico + Instalação por profissional</p><p className="text-xl text-green-300">R$ 750,00</p></div>
+                                <div className="w-96 py-7 minhaimg"><img className="shadoww" src={a} alt="" /> <p className="text-red-600 font-bold">Serviço incluso</p></div>
+                                <div className="text-center lexend-tera w-full"><p className="text-2xl">Kit de Sensores + Instalação por profissional</p><p className="text-xl text-green-300">R$ 750,00</p></div>
                             </div>
                         </CarouselItem>
                         <CarouselItem className="basis-1/3">
                             <div className="flex flex-col cursor-pointer justify-center align-middle items-center">
-                                <div className="w-96 py-7 minhaimg"><img className="shadoww" src={a} alt="" /></div>
-                                <div className="text-center lexend-tera w-full"><p className="text-2xl">Kit Arduino + Sensor sônico + Painel Multimídia + Instalação por profissional</p><p className="text-xl text-green-300">R$ 1500,00</p></div>
+                                <div className="w-96 py-7 minhaimg"><img className="shadoww" src={b} alt="" /><p className="text-red-600 font-bold">Serviço incluso</p></div>
+                                <div className="text-center lexend-tera w-full"><p className="text-2xl">Kit de Sensores + Painel Multimídia + Instalação por profissional</p><p className="text-xl text-green-300">R$ 1500,00</p></div>
                             </div>
                         </CarouselItem>
 
@@ -167,12 +181,65 @@ export default function Home() {
                 </Carousel>
             </section>
 
-            <section id="contact">
 
-            </section>
 
         </main>
-        <footer>
+        <footer id="contato" className="bg-white text-black">
+            <section className="pt-5">
+                <h1 className="text-3xl text-center my-5 lexend-tera">Entre em contato conosco</h1>
+                <div className="">
+                    <form className="form-contact flex justify-evenly" action="https://formsubmit.co/eduardo_cambraia@estudante.sesisenai.org.br" method="POST">
+                        <div id="inputs-div">
+                            <div>
+                                <input type="text" name="nome" className="h-12 rounded-[5px]  p-4 m-4 w-96 text-gray-700" id="w3lName" placeholder="Digite seu Nome:"
+                                    required />
+                            </div>
+                            <div>
+                                <input type="email" className="h-12 rounded-[5px] p-4 m-4 w-96 text-gray-700" name="Email" id="w3lSender" placeholder="Digite seu Email:"
+                                    required /></div>
+                            <div>
+                                <input type="hidden" name="_captcha" value="false" />
+
+                                <textarea className="p-4 m-4 h-48 w-96 resize-none text-gray-700 rounded-[5px]" name="Mensagem" id="w3lMessage"
+                                    placeholder="Escreva aqui sua mensagem" required></textarea>
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-4 ">
+                            <div className="flex flex-col">
+                                <div className="flex flex-row align-middle justify-center mb-2 items-center gap-4">
+                                    <div>
+                                        <SiGmail size={50} className="text-secondaryColor" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>
+                                        <p id="info-contact"> Envie-nos um email manualmente:</p>
+                                    </div>
+                                    <p className="text-center" >duduborges333969@gmail.com</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="flex  flex-col">
+                                    <div className="flex flex-row align-middle justify-center mb-2 items-center gap-4">
+                                        <div>
+                                            <BsWhatsapp size={50} className="text-secondaryColor" />
+                                        </div>
+                                    </div>
+                                    <div className="text-center">
+                                        <div>
+                                            <p id="info-contact"> Ou contate-nos pelo WhatsApp:</p>
+                                        </div>
+                                        <p >(51) 985836686 </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <button className="bg-secondaryColor p-5 rounded-[8px]" >Enviar mensagem</button>
+                        </div>
+
+                    </form>
+                </div>
+            </section>
+
             <div className="bg-white w-full text-black text-3xl flex flex-col align-middle lexend-tera justify-center items-center py-10">
                 <div>
                     <p>DriveSign &copy;</p>
